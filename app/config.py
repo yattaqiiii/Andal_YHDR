854a73cc8963404bb92b069856b516968a848f03
nipqt <hanifahmadsyauqi@gmail.com>
2025-06-03 14:38:34 +0700

    app_mod_fix2

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'kunci_rahasia_super_untuk_doksli_mint_!@#$%^_V8_lebih_aman_lagi'
-    PROJECT_NAME = "doksli mint"
+    PROJECT_NAME = "DOKSLI MINT"
     MAX_DEPTH_LIMIT_DEFAULT = 2
     MAX_PAGES_DEFAULT = 50
     USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 DoksliMintBot/1.0 (+https://github.com/yourusername/dokslimint)'
